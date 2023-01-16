abc54599c438aecb79ebe378c33ff62eae8e65e2
Farhan <farhan.ahmed@learnit.fi>
2023-01-16 09:36:46 +0200

    update the fucntion.js code

--- src/Function.js
+++ src/Function.js
@@ -27,3 +27,29 @@ const Function = () => {
         const y = parseFloat(
           drone.getElementsByTagName("positionY")[0].textContent
         );
+        // Fetch the latest snapshot of drone positions
+      const xml = await axios.get(dronesEndpoint);
+      console.log("Data from Drone Endpoint:", xml.data); // added this
+
+      // Parse the XML data
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(xml.data, "text/xml");
+
+      // Iterate over the drones in the snapshot
+      const drones = xmlDoc.getElementsByTagName("drone");
+      for (const drone of drones) {
+        // Calculate the distance from the nest
+        const distance = Math.sqrt((x - 250000) ** 2 + (y - 250000) ** 2);
+        console.log(distance);
+        // If the drone is within the no-fly zone, fetch the pilot information
+        if (distance <= 100000) {
+          const pilot = await axios.get(pilotsEndpoint + serialNumber);
+          pilot.data.distance = distance;
+          setPilots((p) => [...p, pilot.data]);
+        }
+      }
+    };
+    let intervalId = setInterval(fetchPilots, 10000);
+    return () => clearInterval(intervalId);
+  }, []);
+  return (
