97799b2768e4ddc7e0b03977668b030221c8d839
Farhan <farhan.ahmed@learnit.fi>
2023-01-18 20:39:58 +0200

    Update router setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  createRoot(document.getElementById("root")).render(
+);
